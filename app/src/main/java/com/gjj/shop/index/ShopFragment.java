ff7e7beb4527f242124c3a7a17abba4d6536937f
user <user@userdeMacBook-Air.local>
2016-08-07 00:10:53 +0800

    shouye

---
+++ app/src/main/java/com/gjj/shop/index/ShopFragment.java
@@ -0,0 +1,19 @@
+package com.gjj.shop.index;
+
+import com.gjj.shop.R;
+import com.gjj.shop.base.BaseFragment;
+
+/**
+ * Created by user on 16/8/7.
+ */
+public class ShopFragment extends BaseFragment{
+    @Override
+    public int getContentViewLayout() {
+        return R.layout.fragment_shop;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
