7ef7fb40843387c81a405a7143defdbd077fc509
11hhlin1 <11hhlin1@gmail.com>
2016-08-26 15:40:08 +0800

    增加分类布局

---
+++ app/src/main/java/com/gjj/shop/index/ProductCategoryFragment.java
@@ -0,0 +1,18 @@
+package com.gjj.shop.index;
+
+import com.gjj.shop.base.BaseFragment;
+
+/**
+ * Created by Chuck on 2016/8/26.
+ */
+public class ProductCategoryFragment extends BaseFragment{
+    @Override
+    public int getContentViewLayout() {
+        return 0;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
