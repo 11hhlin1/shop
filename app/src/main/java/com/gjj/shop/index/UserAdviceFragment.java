11d368b2d6de0b51a6b4fded173465718cd392f2
11hhlin1 <11hhlin1@gmail.com>
2016-09-01 17:29:21 +0800

    das

--- app/src/main/java/com/gjj/shop/index/UserAdviceFragment.java
+++ app/src/main/java/com/gjj/shop/index/UserAdviceFragment.java
@@ -4,10 +4,17 @@ import com.gjj.shop.R;
 import com.gjj.shop.base.BaseFragment;
 import com.gjj.shop.base.BaseSubActivity;
 
+import butterknife.Bind;
+import cn.finalteam.loadingviewfinal.PtrClassicFrameLayout;
+import cn.finalteam.loadingviewfinal.RecyclerViewFinal;
+
 /**
  * Created by user on 16/8/27.
  */
 public class UserAdviceFragment extends BaseFragment{
+    @Bind(R.id.recyclerView)
+    RecyclerViewFinal mRecyclerView;
+
     @Override
     public int getContentViewLayout() {
         return R.layout.fragment_user_advice;
